96fab028f7e965ad733288c0e8f2bfbc12f7f982
BaDjinn <g.sottovia@yahoo.com>
2023-11-16 22:24:41 +0100

    PrimeNumbers

---
+++ be/server.js
@@ -0,0 +1,52 @@
+//header
+//librerie
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+//variabili d'ambiente
+const PORT = process.env.REACT_APP_SERVERPORT;
+
+// Aggiungo le rotte
+//const userRoute = require("./routes/userRoute");
+
+//Importo eventuali middlewares
+//const logger = require("../middlewares/logger");
+
+//inizializzo app/express
+const app = express();
+//app.use per i middleware
+app.use(express.json());
+app.use(cors());
+
+//app.use per le routes
+/*app.use("/", userRoute);*/
+
+//creo la risposta per la root principale
+app.get("/", (req, res) => {
+	res.send({
+		//dati della risposta sulla get principale
+	});
+});
+
+//inizializzo sul serio mongoose
+mongoose.connect(process.env.REACT_APP_MONGOOSESERVERCONNECTION, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+//lancio la connessione con il db di moongoose
+const db = mongoose.connection;
+// db.on  vuol dire, ogni volta che
+db.on("error", console.error.bind(console, `Error during DB connec--`));
+// db.once vuol dire, una volta che
+db.once("open", () => {
+	const now = new Date();
+	console.log(`Database suxex and connected now: ${now}`);
+});
+
+//metto l'app in ascolto sulla porta che ho stabilito in ".env"
+app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`));
+
+console.log("Got ya!");
