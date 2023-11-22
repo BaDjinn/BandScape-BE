2544a22dbeadcf41330801781509168a88b3585c
BaDjinn <44167536+BaDjinn@users.noreply.github.com>
2023-11-22 17:33:45 +0100

    7thSigil

--- be/models/commentsModel.js
+++ be/models/commentsModel.js
@@ -1,25 +1,28 @@
 const mongoose = require("mongoose");
 
-const CommentSchema = new mongoose.Schema({
-	postFather: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "postModel",
-		type: String,
-		required: true,
+const CommentSchema = new mongoose.Schema(
+	{
+		postFather: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "postModel",
+			type: String,
+			required: true,
+		},
+		commentFather: {
+			type: String,
+			required: false,
+		},
+		author: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "userModel",
+			required: true,
+		},
+		content: {
+			type: String,
+			required: true,
+		},
 	},
-	commentFather: {
-		type: String,
-		required: false,
-	},
-	author: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "userModel",
-		required: true,
-	},
-	content: {
-		type: String,
-		required: true,
-	},
-});
+	{ timestamp: true, strict: true }
+);
 
 module.exports = mongoose.model("commentModel", CommentSchema, "comment");
